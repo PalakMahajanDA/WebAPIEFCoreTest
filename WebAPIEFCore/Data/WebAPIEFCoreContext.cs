51cc83c08e1648ac73b0d6c265a79f4acae1533f
palakm <39305866+palakm@users.noreply.github.com>
2018-05-24 11:55:00 +0530

    Add project files.

---
+++ WebAPIEFCore/Data/WebAPIEFCoreContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPIEFCore.Models
+{
+    public class WebAPIEFCoreContext : DbContext
+    {
+        public WebAPIEFCoreContext (DbContextOptions<WebAPIEFCoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAPIEFCore.Models.Employees> Employees { get; set; }
+    }
+}
